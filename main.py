7350bf604067861808763ad68667a4ed80a34af5
Zeststudi <mishakomarov57@gmail.com>
2021-11-24 10:02:47 +0300

    xx

---
+++ main.py
@@ -0,0 +1,25 @@
+alfavit = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+smeshenie = int(input('Шаг шифровки: '))
+message = input("Сообщение для шифровки: ").upper()
+itog = ''
+for i in message:
+    mesto = alfavit.find(i)
+    new_mesto = mesto + smeshenie
+    if i in alfavit:
+        itog += alfavit[new_mesto]
+    else:
+        itog += i
+print(itog)
+
+alfavit = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+smeshenie = int(input('Шаг шифровки: '))
+message = input("Сообщение для ДЕшифровки: ").upper()
+itog = ''
+for i in message:
+    mesto = alfavit.find(i)
+    new_mesto = mesto - smeshenie
+    if i in alfavit:
+        itog += alfavit[new_mesto]
+    else:
+        itog += i
+print(itog)
